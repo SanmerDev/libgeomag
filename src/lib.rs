5cf620ab81fa0d7b9b478f71e822720f281bfb86
SanmerDev <sanmer@sanmer.dev>
2023-11-02 11:30:31 +0800

    Rename `Prime` to `Vector`

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ mod polynomial;
 pub mod util;
 
 #[derive(Default, Debug)]
-struct Prime {
+struct Vector {
     x: f64,
     y: f64,
     z: f64,
@@ -22,25 +22,25 @@ struct Prime {
     dz: f64,
 }
 
-impl From<Prime> for MagneticField {
-    fn from(p: Prime) -> Self {
-        let h = (p.x.powi(2) + p.y.powi(2)).sqrt();
-        let f = (h.powi(2) + p.z.powi(2)).sqrt();
-        let i = (p.z / h).atan();
-        let d = (p.y / p.x).atan();
+impl From<Vector> for MagneticField {
+    fn from(v: Vector) -> Self {
+        let h = (v.x.powi(2) + v.y.powi(2)).sqrt();
+        let f = (h.powi(2) + v.z.powi(2)).sqrt();
+        let i = (v.z / h).atan();
+        let d = (v.y / v.x).atan();
 
-        let dh = (p.x * p.dx + p.y * p.dy) / h;
-        let df = (p.x * p.dx + p.y * p.dy + p.z * p.dz) / f;
-        let di = (h * p.dz - p.z * dh) / f.powi(2);
-        let dd = (p.x * p.dy - p.y * p.dx) / h.powi(2);
+        let dh = (v.x * v.dx + v.y * v.dy) / h;
+        let df = (v.x * v.dx + v.y * v.dy + v.z * v.dz) / f;
+        let di = (h * v.dz - v.z * dh) / f.powi(2);
+        let dd = (v.x * v.dy - v.y * v.dx) / h.powi(2);
 
         MagneticField {
-            x: p.x,
-            x_dot: p.dx,
-            y: p.y,
-            y_dot: p.dy,
-            z: p.z,
-            z_dot: p.dz,
+            x: v.x,
+            x_dot: v.dx,
+            y: v.y,
+            y_dot: v.dy,
+            z: v.z,
+            z_dot: v.dz,
             h,
             h_dot: dh,
             f,
@@ -76,20 +76,20 @@ impl<T: Gauss> Geomag<T> {
                 d *= i as f64;
             }
             let c = 1. / d;
+
             pnm * (2.0 * c).sqrt()
         } else {
             pnm
         }
     }
 
-    fn xyz_prime(&self, n: usize) -> Prime {
-        let mut prime = Prime::default();
+    fn xyz_prime(&self, n: usize) -> Vector {
+        let mut vector = Vector::default();
 
         let r = self.location.radius;
         let p = self.location.latitude;
         let l = self.location.longitude;
         let a = 6371200_f64;
-
         let ps = p.sin();
         let pc = p.cos();
 
@@ -112,41 +112,40 @@ impl<T: Gauss> Geomag<T> {
                 let hcl = self.gauss.h(n, m) * mlc;
                 let hsl = self.gauss.h(n, m) * mls;
 
-                prime.x += -f * (gcl + hsl) * dmn;
-                prime.y += (f / pc) * mf * (gsl - hcl) * pmn;
-                prime.z += -f * (nf + 1.0) * (gcl + hsl) * pmn;
+                vector.x += -f * (gcl + hsl) * dmn;
+                vector.y += (f / pc) * mf * (gsl - hcl) * pmn;
+                vector.z += -f * (nf + 1.0) * (gcl + hsl) * pmn;
 
                 let d_gcl = self.gauss.dg(n, m) * mlc;
                 let d_gsl = self.gauss.dg(n, m) * mls;
                 let d_hcl = self.gauss.dh(n, m) * mlc;
                 let d_hsl = self.gauss.dh(n, m) * mls;
 
-                prime.dx += (-f) * (d_gcl + d_hsl) * dmn;
-                prime.dy += (f / pc) * mf * (d_gsl - d_hcl) * pmn;
-                prime.dz += -f * (nf + 1.0) * (d_gcl + d_hsl) * pmn;
+                vector.dx += (-f) * (d_gcl + d_hsl) * dmn;
+                vector.dy += (f / pc) * mf * (d_gsl - d_hcl) * pmn;
+                vector.dz += -f * (nf + 1.0) * (d_gcl + d_hsl) * pmn;
             }
         }
 
-        prime
+        vector
     }
 
-    fn xyz(&self, n: usize) -> Prime {
-        let mut xyz = Prime::default();
-        let prime = self.xyz_prime(n);
+    fn xyz(&self, n: usize) -> Vector {
+        let mut xyz = Vector::default();
+        let vector = self.xyz_prime(n);
 
         let p1 = self.location.latitude;
         let p = self.location.inner.latitude;
-
         let sin_p = (p1 - p).sin();
         let cos_p = (p1 - p).cos();
 
-        xyz.x = prime.x * cos_p - prime.z * sin_p;
-        xyz.y = prime.y;
-        xyz.z = prime.x * sin_p + prime.z * cos_p;
+        xyz.x = vector.x * cos_p - vector.z * sin_p;
+        xyz.y = vector.y;
+        xyz.z = vector.x * sin_p + vector.z * cos_p;
 
-        xyz.dx = prime.dx * cos_p - prime.dz * sin_p;
-        xyz.dy = prime.dy;
-        xyz.dz = prime.dx * sin_p + prime.dz * cos_p;
+        xyz.dx = vector.dx * cos_p - vector.dz * sin_p;
+        xyz.dy = vector.dy;
+        xyz.dz = vector.dx * sin_p + vector.dz * cos_p;
 
         xyz
     }
